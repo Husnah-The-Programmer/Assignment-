e5cd22f1fcf119e5b371e087936e4ddd31e0a382
Husnah-The-Programmer <67584836+Husnah-The-Programmer@users.noreply.github.com>
2020-10-01 13:00:25 +0100

    Add files via upload

---
+++ Assignment.java
@@ -0,0 +1,32 @@
+public class Assignment {
+    public static void main(String args[]) {
+        char selection;
+        System.out.println("Question 1\n");
+        System.out.println("C programs are converted into machine language with the help of________?\n");
+        System.out.println("A. An Editor\n");
+        System.out.println("B. A Compiler\n");
+        System.out.println("C. An Operating System\n");
+        System.out.println("D. None of the above\n");
+        System.out.println("E. Quit\n");
+        scanf("%s", selection);
+        
+        if (selection == 'A'){
+            println("Incorrect\n");
+        }
+        else if (selection == 'B'){
+            println("Correct\n");
+        }
+        else if (selection == 'C'){
+            println("Incorrect\n");
+        }
+        else if (selection == 'D'){
+            println("Incorrect\n"); 
+        }
+        else{
+            println("Wrong choice, terminating the program.\n");
+        }
+           
+
+    }
+
+}    
